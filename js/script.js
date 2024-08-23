58569cb335fdb11c60cc995801742b0c9292f962
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-08-22 22:35:54 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-const menuHamburguer=document.querySelector(".menu-hamburguer");menuHamburguer.addEventListener("click",()=>{toggleMenu()});function toggleMenu(){const a=document.querySelector(".nav-responsive");menuHamburguer.classList.toggle("change"),a.style.display=menuHamburguer.classList.contains("change")?"block":"none"}
+/*  turns the menu hamburguer into a X ==== */
+
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+menuHamburguer.addEventListener('click', () => {
+
+    toggleMenu();
+});
+
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block';
+
+    } else {
+        nav.style.display = 'none';
+    }
+}
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('Button clicked!'); // Adicione este log para verificar
+
+        const serviceBox = button.closest('.services-box');
+        serviceBox.classList.toggle('expanded');
+
+        if (serviceBox.classList.contains('expanded')) {
+            button.textContent = 'Leia menos';
+        } else {
+            button.textContent = 'Leia mais';
+        }
+    });
+});
