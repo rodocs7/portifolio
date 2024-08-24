834dc23ba6273501370c789a105024eaf750c9d4
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-08-24 13:50:31 -0300

    index js

--- js/script.js
+++ js/script.js
@@ -37,66 +37,67 @@ function toggleMenu() {
 
 
 class FormSubmit {
-  constructor(settings) {
-    this.settings = settings;
-    this.form = document.querySelector(settings.form);
-    this.formButton = document.querySelector(settings.button);
-    if (this.form) {
-      this.url = this.form.getAttribute("action");
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
     }
-    this.sendForm = this.sendForm.bind(this);
-  }
-
-  displaySuccess() {
-    this.form.innerHTML = this.settings.success;
-  }
-
-  displayError() {
-    this.form.innerHTML = this.settings.error;
-  }
-
-  getFormObject() {
-    const formObject = {};
-    const fields = this.form.querySelectorAll("[name]");
-    fields.forEach((field) => {
-      formObject[field.getAttribute("name")] = field.value;
-    });
-    return formObject;
-  }
-
-  onSubmission(event) {
-    event.preventDefault();
-    event.target.disabled = true;
-    event.target.innerText = "Enviando...";
-  }
-
-  async sendForm(event) {
-    try {
-      this.onSubmission(event);
-      const formData = new FormData(this.form); // Corrigido para usar FormData
-
-      await fetch(this.url, {
-        method: "POST",
-        body: formData,
+  
+    displaySuccess() {
+      this.form.innerHTML = this.settings.success;
+    }
+  
+    displayError() {
+      this.form.innerHTML = this.settings.error;
+    }
+  
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
       });
-      this.displaySuccess();
-    } catch (error) {
-      this.displayError();
-      console.error(error); // Ajuste para capturar e registrar erros
+      return formObject;
+    }
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        const formData = new FormData(this.form); // Corrigido para usar FormData
+  
+        await fetch(this.url, {
+          method: "POST",
+          body: formData,
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        console.error(error); // Ajuste para capturar e registrar erros
+      }
+    }
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
     }
   }
-
-  init() {
-    if (this.form) this.formButton.addEventListener("click", this.sendForm);
-    return this;
-  }
-}
-
-const formSubmit = new FormSubmit({
-  form: "[data-form]",
-  button: "[data-button]",
-  success: "<h1 class='success'>Mensagem enviada!</h1>",
-  error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-});
-
-formSubmit.init();
+  
+  const formSubmit = new contact({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  
+  formSubmit.init();
+  
