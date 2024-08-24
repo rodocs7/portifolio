39fed6fdc99ed33e8d9e7bfe6ca9fcbc9a1d7f8c
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-08-24 10:30:40 -0300

    up

--- js/script.js
+++ js/script.js
@@ -34,69 +34,75 @@ function toggleMenu() {
 //         }
 //     });
 // });
-class contact {
-    constructor(settings) {
-      this.settings = settings;
-      this.form = document.querySelector(settings.form);
-      this.formButton = document.querySelector(settings.button);
-      if (this.form) {
-        this.url = this.form.getAttribute("action");
-      }
-      this.sendForm = this.sendForm.bind(this);
-    }
+
+
+
+
+
+
+// class contact {
+//     constructor(settings) {
+//       this.settings = settings;
+//       this.form = document.querySelector(settings.form);
+//       this.formButton = document.querySelector(settings.button);
+//       if (this.form) {
+//         this.url = this.form.getAttribute("action");
+//       }
+//       this.sendForm = this.sendForm.bind(this);
+//     }
   
-    displaySuccess() {
-      this.form.innerHTML = this.settings.success;
-    }
+//     displaySuccess() {
+//       this.form.innerHTML = this.settings.success;
+//     }
   
-    displayError() {
-      this.form.innerHTML = this.settings.error;
-    }
+//     displayError() {
+//       this.form.innerHTML = this.settings.error;
+//     }
   
-    getFormObject() {
-      const formObject = {};
-      const fields = this.form.querySelectorAll("[name]");
-      fields.forEach((field) => {
-        formObject[field.getAttribute("name")] = field.value;
-      });
-      return formObject;
-    }
+//     getFormObject() {
+//       const formObject = {};
+//       const fields = this.form.querySelectorAll("[name]");
+//       fields.forEach((field) => {
+//         formObject[field.getAttribute("name")] = field.value;
+//       });
+//       return formObject;
+//     }
   
-    onSubmission(event) {
-      event.preventDefault();
-      this.formButton.disabled = true;
-      this.formButton.innerText = "Enviando...";
-    }
+//     onSubmission(event) {
+//       event.preventDefault();
+//       this.formButton.disabled = true;
+//       this.formButton.innerText = "Enviando...";
+//     }
   
-    async sendForm(event) {
-      try {
-        this.onSubmission(event);
-        await fetch(this.url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify(this.getFormObject()),
-        });
-        this.displaySuccess();
-      } catch (error) {
-        this.displayError();
-        throw new Error(error);
-      }
-    }
+//     async sendForm(event) {
+//       try {
+//         this.onSubmission(event);
+//         await fetch(this.url, {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//           },
+//           body: JSON.stringify(this.getFormObject()),
+//         });
+//         this.displaySuccess();
+//       } catch (error) {
+//         this.displayError();
+//         throw new Error(error);
+//       }
+//     }
   
-    init() {
-      if (this.form) this.formButton.addEventListener("click", this.sendForm);
-      return this;
-    }
-  }
+//     init() {
+//       if (this.form) this.formButton.addEventListener("click", this.sendForm);
+//       return this;
+//     }
+//   }
   
-  const formSubmit = new Contact({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-  });
-  formSubmit.init();
+//   const formSubmit = new Contact({
+//     form: "[data-form]",
+//     button: "[data-button]",
+//     success: "<h1 class='success'>Mensagem enviada!</h1>",
+//     error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+//   });
+//   formSubmit.init();
   
