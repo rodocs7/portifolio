404c030f3c97044207f3086d3225f5d793edbe7c
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-08-24 08:40:17 -0300

    form

--- js/script.js
+++ js/script.js
@@ -19,17 +19,21 @@ function toggleMenu() {
         nav.style.display = 'none';
     }
 }
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        console.log('Button clicked!'); // Adicione este log para verificar
 
-        const serviceBox = button.closest('.services-box');
-        serviceBox.classList.toggle('expanded');
+// document.querySelectorAll('.btn').forEach(button => {
+//     button.addEventListener('click', () => {
+//         console.log('Button clicked!'); // Adicione este log para verificar
 
-        if (serviceBox.classList.contains('expanded')) {
-            button.textContent = 'Leia menos';
-        } else {
-            button.textContent = 'Leia mais';
-        }
-    });
-});
+//         const serviceBox = button.closest('.services-box');
+//         serviceBox.classList.toggle('expanded');
+
+//         if (serviceBox.classList.contains('expanded')) {
+//             button.textContent = 'Leia menos';
+//         } else {
+//             button.textContent = 'Leia mais';
+//         }
+//     });
+// });
+
+
+class contact {}
